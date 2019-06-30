0129eba1fd73dae0ed24fb3274d17fb004181ed7
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-06-30 13:50:05 -0400

    Change class to OOP to make more sense

---
+++ OOP.py
@@ -0,0 +1,35 @@
+###Information and questions for OOP
+
+#1 Instance
+"""An instance is what the initial object is. 
+Ex: a dog instance needs to be created before you can assign the details about it"""
+
+#2 Classes
+"""An object is an instance of a class"""
+
+#3 Methods
+"""Functions that are defined in a class"""
+
+#4 Initializer
+"""Things that are run first when a class object is called"""
+
+#5 Encapsulation
+"""Bundling data and methods that work on that data within one unit (Ex. a class)"""
+
+#6 Subclasses
+"""The children classes of a parent class"""
+
+#7 Superclasses
+"""The parent class that has children classes"""
+
+#8 Inheritance
+"""Allows new classes to inherit properties and methods of existing classes."""
+
+#9 Polymorphism
+"""Ability for different data types to use the same data structure. 
+(Ex. different shapes can inherit from a single shapes class because they share the same features)"""
+
+#10 Composition
+"""Classes should use polymorphism to reuse code by composition rather than inheritance"""
+
+
--- classes.py
+++
@@ -1,8 +0,0 @@
-###Information and questions for classes
-
-#1 Instance
-"""An instance is what the initial object is. 
-Ex: a dog instance needs to be created before you can assign the details about it"""
-
-#2 Classes
-"""An object is an instance of a class"""
