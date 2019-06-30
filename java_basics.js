60e1d78539095f38e633c9a4dc7c99a9decd830e
brandonmino <43628774+brandonmino@users.noreply.github.com>
2019-06-30 15:55:11 -0400

    Added java section

---
+++ java_basics.js
@@ -0,0 +1,16 @@
+/*Basics of Java(script)
+
+#1 Arrow Functions:
+reg func: 
+function funcName(parameters) {
+    return parameters + 2;
+}
+arrow func:
+var funcName = (parameters) => parameters + 2
+
+#2 This:
+Value of This is usually determined by how a function is called.
+This might be different each time it is called.
+
+*/
+
